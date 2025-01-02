43784d476f0bc25b3c60cb968699069354d55007
m3et2001 <meetvelani2728@gmail.com>
2025-01-02 21:59:32 +0530

    a

--- cronjob.js
+++ cronjob.js
@@ -32,8 +32,8 @@ import { contractAwardAllListForCron } from "./app/features/contract_award/contr
 
 // this cron is related to send the mail to user about the tenders
 
-cron.schedule('0 0 * * *', async function () {       // midnight
-// cron.schedule("*/10 * * * * *", async function () { // testing
+// cron.schedule('0 0 * * *', async function () {       // midnight
+cron.schedule("*/10 * * * * *", async function () { // testing
   try {
     console.log("Cron job started...");
     console.log("Fetching active customers with tenders_filter...");
